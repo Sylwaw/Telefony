6a163a61897d4e9b613553b946a39aaac5580061
Sylwaw <38911916+Sylwaw@users.noreply.github.com>
2018-05-06 16:14:35 +0200

    New version
    All new

--- main.c
+++ main.c
@@ -1,31 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct
+#include <string.h>
+typedef struct LINIJKA linijka;
+struct LINIJKA
 {
+    char osoba[30];
+    char telefon[20];
+    linijka* next;
+};
 
+void WczytajZPliku(linijka* glowa, char *nazwa)
+{
+    linijka *tmp, *nowy;
+    //tmp = glowa;
+    char bufor[30+1];
+    FILE* plik = fopen(nazwa, "r");
+    if(plik == NULL)
+    {
+        printf("Blad odczytu pliku");
+    }
+    else
+    {
+        while(fscanf(plik, "%s", bufor) != EOF)
+        {
+            nowy = (linijka*)malloc(sizeof(linijka));
+            nowy->next = NULL;
+            strcpy(nowy->osoba, bufor);
+            fscanf(plik, "%s", bufor);
+			strcpy(nowy->telefon, bufor);
+
+			if (glowa == NULL)
+            {
+                nowy = glowa;
+            }
+            else
+            {
+                tmp = glowa;
+                while (tmp->next != NULL)
+					tmp = tmp->next;
+				tmp->next = nowy;
+            }
+
+        }
+        printf("Plik zostal poprawnie wczytany");
+    }
+    fclose(plik);
 }
 
 int main()
 {
-    printf("Hello world!\n");
+    linijka* glowa = NULL;
+    WczytajZPliku(glowa, "plik.txt");
+
     return 0;
 }
 
-int nowaFunkcja(int a) {
-    return a;
-}
 
-int wczytaniePliku(char nazwa)//wczytuje plik
-{
-    FILE *plik;
-    if((plik = fopen(nazwa, "r")) == NULL)
-    {
-        printf("Blad odczytu plikuu");
-    }
 
-    else
-    {
-        fscansf()
-    }
-}
